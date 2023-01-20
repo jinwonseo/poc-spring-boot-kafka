0c277055f49ce8b4c04c7a06f9a0d6cca575170a
Jinwon Seo <jwseo@live.co.kr>
2023-01-20 10:23:45 +0900

    Add produce method

--- src/main/java/kr/sprouts/sandbox/kafka/service/ProducerService.java
+++ src/main/java/kr/sprouts/sandbox/kafka/service/ProducerService.java
@@ -1,10 +1,15 @@
 package kr.sprouts.sandbox.kafka.service;
 
 import kr.sprouts.sandbox.kafka.dto.TestRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Service
+@Slf4j
 public class ProducerService {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
@@ -15,5 +20,19 @@ public class ProducerService {
 
     public void produce(TestRequest request) {
 
+        ListenableFuture<SendResult<String, Object>> future =
+                this.kafkaTemplate.send("spring-boot-kafka-dto", request);
+
+        future.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error(ex.getLocalizedMessage());
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, Object> result) {
+                log.info(result.toString());
+            }
+        });
     }
 }
