f280bd909015c7537fb9fd10e866b343d2d9ac59
Jinwon Seo <jwseo@live.co.kr>
2023-01-20 10:20:34 +0900

    Inject kafka template

--- src/main/java/kr/sprouts/sandbox/kafka/service/ProducerService.java
+++ src/main/java/kr/sprouts/sandbox/kafka/service/ProducerService.java
@@ -1,10 +1,18 @@
 package kr.sprouts.sandbox.kafka.service;
 
 import kr.sprouts.sandbox.kafka.dto.TestRequest;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProducerService {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public ProducerService(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     public void produce(TestRequest request) {
 
     }
