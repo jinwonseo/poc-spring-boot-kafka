86c4b8d06ebc9fea7f8980496fb576e23eb2f0de
Jinwon Seo <jwseo@live.co.kr>
2023-01-26 11:29:56 +0900

    Add property SPECIFIC_AVRO_READER_CONFIG=true

--- src/main/java/kr/sprouts/sandbox/kafka/service/ConsumerService.java
+++ src/main/java/kr/sprouts/sandbox/kafka/service/ConsumerService.java
@@ -1,8 +1,17 @@
 package kr.sprouts.sandbox.kafka.service;
 
+import kr.sprouts.sandbox.kafka.dto.schema.TestSchema;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ConsumerService {
-    
+
+    @KafkaListener(topics = "spring-boot-kafka-avsc")
+    public void consume(ConsumerRecord<String, TestSchema> record) {
+        log.info(record.value().getName());
+    }
 }
